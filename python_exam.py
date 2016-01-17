346ba8311794043856c3c903a213118ce89d9e1f
changmo <ncm4114@gmail.com>
2016-01-18 00:15:05 +0900

    word preprocessing exam

--- python_exam.py
+++ python_exam.py
@@ -1,79 +1,55 @@
-import os
-#import tensorflow as tf
-#import time
 import numpy as np
 import pandas as pd
+import os
+from keras.preprocessing import sequence
 
 data_path = '/data3/flickr30k/'
-feat_path = data_path + 'feats.npy'
-annotation_path = os.path.join(data_path, 'results_20130124.token')
-# word vector
+annotation_path = os.path.join(data_path, 'results_20130124.token') # pickle
 
-# using Pandas
-def get_caption_data(annotation_path, feat_path):
-     # .npy ==> np.load(path)
-     feats = np.load(feat_path)
-     # Pandas read_table ==> read data as DataFormat in Pandas
-     annotations = pd.read_table(annotation_path, sep='\t', header=None, names=['image', 'caption'])
-     captions = annotations['caption'].values
+annotations = pd.read_table(annotation_path, sep='\t', header=None, names=['image', 'caption'])
+captions = annotations['caption'].values
 
-     return feats, captions
+word2idx = np.load('/data3/flickr30k/word2idx.npy').tolist()
+feats = np.load('/data3/flickr30k/feats.npy')
 
-# From NeuralTalk
-def preProBuildWordVocab(sentence_iterator, word_count_threshold=30): # borrowed this function from NeuralTalk
-     print 'preprocessing word counts and creating vocab based on word count threshold %d' % (word_count_threshold, )
-     word_counts = {}
+index = np.arange(len(feats))
+np.random.shuffle(index)
 
-     nsents = 0
-     for sent in sentence_iterator:
-        nsents += 1
-        # str.lower() : return a copy of the string with all the cased characters ...
-        #                converted to lowercase
-        # str.split(' ') : word separation
-        for w in sent.lower().split(' '):
-            # w : string type & one word
-            # get(key[, default]) : return the value for key if key is in the dict, else default.
-            # word_counts : dict type
-            word_counts[w] = word_counts.get(w, 0) + 1
-
-     # total word_counts length : 20326 // vocab length : 2942 (filtered ... > 30)
-     vocab = [w for w in word_counts if word_counts[w] >= word_count_threshold]
-     # vocab ==> list type. & word set.
-     # vocab[0] ... yellow
-     print 'filtered words from %d to %d' % (len(word_counts), len(vocab))
+feats = feats[index]
+captions = captions[index]
 
 
-     ixtoword = {} # dict type
-     ixtoword[0] = '.'  # period at the end of the sentence. make first dimension be end token
-     wordtoix = {}
-     wordtoix['#START#'] = 0 # make first vector be the start token
-     ix = 1
-     for w in vocab:
-       wordtoix[w] = ix # dictionary type ==> {'four': 2, 'sleep': 2206, ......}
-       ixtoword[ix] = w # dictionary type ==> {0: ',', 1:'yellow', ......}
-       ix += 1
-
-     word_counts['.'] = nsents
-     bias_init_vector = np.array([1.0*word_counts[ixtoword[i]] for i in ixtoword])
-     bias_init_vector /= np.sum(bias_init_vector) # normalize to frequencies
-     bias_init_vector = np.log(bias_init_vector)
-     bias_init_vector -= np.max(bias_init_vector) # shift to nice numeric range
-     return wordtoix, ixtoword, bias_init_vector
+maxlen = 30
+batch_size = 3
+len_feat = batch_size * 2
 
 
-feats, captions = get_caption_data(annotation_path, feat_path)
-wordtoix, ixtoword, bias_init_vector = preProBuildWordVocab(captions)
+for start, end in zip(range(0,len_feat,batch_size),range(batch_size,len_feat,batch_size)):
+    # start, end : 0,3
+    current_feats = feats[start:end] # (batch_size,4096)
+    current_captions = captions[start:end]
 
-print type(captions)
-print captions.shape
-print captions
+    #current_caption_ind : list type / len(curr~) : 3
+    current_caption_ind = map(lambda cap: [word2idx[word] for word in cap.lower().split(' ')[:-1] if word in word2idx], current_captions)
 
-print type(feats)
-print feats.shape
 
-print type(wordtoix)
-print wordtoix
-print type(ixtoword)
-print ixtoword
-print type(bias_init_vector)
-print bias_init_vector
+    # current_caption_matrix : ndarray / shape : (3,31)
+    current_caption_matrix = sequence.pad_sequences(current_caption_ind, padding='post', maxlen=maxlen+1) # ndarray
+
+    # current_caption_matrix : ndarray / shape : (3,32) / front => 0
+    current_caption_matrix = np.hstack( [np.full( (len(current_caption_matrix),1), 0), current_caption_matrix] ).astype(int)
+
+    # current_caption_matrix.shape[0] : batch_size
+    # current_caption_matrix.shape[1] : maxlen = maxlen + 1
+    current_mask_matrix = np.zeros((current_caption_matrix.shape[0], current_caption_matrix.shape[1]))
+
+    # nonzeros : ndarray / (3,)
+    nonzeros = np.array( map(lambda x: (x != 0).sum()+2, current_caption_matrix ))
+
+
+    for ind, row in enumerate(current_mask_matrix):
+        print (ind,row)
+        print nonzeros[ind]
+        row[:nonzeros[ind]] = 1
+        print row
+        print type(row)
